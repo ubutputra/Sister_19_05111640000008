b16fb16fd614c524c9e7a32536a9ec50153efa9c
ubutputra <ubut31@gmail.com>
2019-10-13 13:48:14 +0700

    hiya

--- tugas 2 pyro/greet_server.py
+++ tugas 2 pyro/greet_server.py
@@ -14,8 +14,10 @@ def start_with_ns():
     #name server harus di start dulu dengan  pyro4-ns -n localhost -p 7777
     #gunakan URI untuk referensi name server yang akan digunakan
     #untuk mengecek service apa yang ada di ns, gunakan pyro4-nsc -n localhost -p 7777 list
-    daemon = Pyro4.Daemon(host="localhost")
-    ns = Pyro4.locateNS("localhost",7777)
+     #multi device tinggal ganti localhost jadi ip, client dan server pakai ip yg sama
+    # jalankan command pyro4-ns -n ipmu -p 7777
+    daemon = Pyro4.Daemon(host='10.151.254.50')
+    ns = Pyro4.locateNS("10.151.254.50",7777)
     x_GreetServer = Pyro4.expose(FileManage)
     uri_greetserver = daemon.register(x_GreetServer)
     print("URI greet server : ", uri_greetserver)
--- tugas 2 pyro/sister2019_contoh/c0/greet_server.py
+++ tugas 2 pyro/sister2019_contoh/c0/greet_server.py
@@ -14,6 +14,7 @@ def start_with_ns():
     #name server harus di start dulu dengan  pyro4-ns -n localhost -p 7777
     #gunakan URI untuk referensi name server yang akan digunakan
     #untuk mengecek service apa yang ada di ns, gunakan pyro4-nsc -n localhost -p 7777 list
+   
     daemon = Pyro4.Daemon(host="localhost")
     ns = Pyro4.locateNS("localhost",7777)
     x_GreetServer = Pyro4.expose(GreetServer)
